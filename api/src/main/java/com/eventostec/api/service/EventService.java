3b99496a6dc96c59c654dce0abc1f93f41de90c1
VDRBreno <vdr.breno0149@outlook.com>
2024-08-01 19:40:51 -0300

    Modify/Create Event

--- api/src/main/java/com/eventostec/api/service/EventService.java
+++ api/src/main/java/com/eventostec/api/service/EventService.java
@@ -1,20 +1,25 @@
 package com.eventostec.api.service;
 
-import java.sql.Date;
+import java.util.Date;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.eventostec.api.domain.event.Event;
 import com.eventostec.api.domain.event.EventRequestDTO;
+import com.eventostec.api.domain.event.EventResponseDTO;
 import com.eventostec.api.repositories.EventRepository;
 
 @Service
 public class EventService {
 
     @Autowired
-    private EventRepository repository;
+    private EventRepository eventRepository;
 
     public Event createEvent(EventRequestDTO data) {
         String imgUrl = null;
@@ -31,9 +36,25 @@ public class EventService {
         newEvent.setImgUrl(imgUrl);
         newEvent.setRemote(data.remote());
 
-        repository.save(newEvent);
+        eventRepository.save(newEvent);
 
         return newEvent;
+    }
+
+    public List<EventResponseDTO> getUpcomingEvents(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        Page<Event> eventsPages = this.eventRepository.findUpcomingEvents(new Date(), pageable);
+        return eventsPages.map(event -> new EventResponseDTO(
+            event.getId(),
+            event.getTitle(),
+            event.getDescription(),
+            event.getDate(),
+            "",
+            "",
+            event.getRemote(),
+            event.getEventUrl(),
+            event.getImgUrl()
+        )).toList();
     }
 
     // No projeto original é feito com conexão ao AWS S3, mas não usarei AWS, logo não haverá upload
