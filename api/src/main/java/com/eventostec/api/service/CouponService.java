b17ef0139e1964fe6a7f99075f7d57cf4596d118
VDRBreno <vdr.breno0149@outlook.com>
2024-08-01 19:41:00 -0300

    Create Coupon

---
+++ api/src/main/java/com/eventostec/api/service/CouponService.java
@@ -0,0 +1,39 @@
+package com.eventostec.api.service;
+
+import java.util.Date;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.eventostec.api.domain.coupon.Coupon;
+import com.eventostec.api.domain.coupon.CouponRequestDTO;
+import com.eventostec.api.domain.event.Event;
+import com.eventostec.api.repositories.CouponRepository;
+import com.eventostec.api.repositories.EventRepository;
+
+@Service
+public class CouponService {
+
+    @Autowired
+    private CouponRepository couponRepository;
+
+    @Autowired
+    private EventRepository eventRepository;
+
+    public Coupon addCouponToEvent(UUID eventId, CouponRequestDTO data) {
+
+        Event event = eventRepository.findById(eventId)
+            .orElseThrow(() -> new IllegalArgumentException("Event not found"));
+
+        Coupon coupon = new Coupon();
+        coupon.setCode(data.code());
+        coupon.setDiscount(data.discount());
+        coupon.setValid(new Date(data.valid()));
+        coupon.setEvent(event);
+
+        return couponRepository.save(coupon);
+
+    }
+
+}
